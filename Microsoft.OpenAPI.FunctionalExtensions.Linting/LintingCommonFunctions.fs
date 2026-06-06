9432b97bb385be4d0b4a5feddd0c4dd5422f8cf1
Vladimir Rogozhin <vladimirrogozhin90@gmail.com>
2026-06-06 17:41:05 +0200

    Remove legacy dead code from OpenApiTools (traverseSchema, getOperations, intersect)

--- Microsoft.OpenAPI.FunctionalExtensions.Linting/LintingCommonFunctions.fs
+++ Microsoft.OpenAPI.FunctionalExtensions.Linting/LintingCommonFunctions.fs
@@ -4,10 +4,9 @@ open System
 open ResultEx
 open Microsoft.OpenApi
 open Microsoft.OpenAPI.FunctionalExtensions.Linting.Types
+open Microsoft.OpenAPI.FunctionalExtensions.OpenApiAdapters
 
-let getSchemaByName (document:OpenApiDocument) (schemaName:String) =
-  schemaName
-  |> OpenApiTools.getSchemaByName document
-  |> function
+let getSchemaByName (document: OpenApiDocument) (schemaName: string) =
+  match tryGetComponentSchema document schemaName with
   | Some schema -> schema |> Ok
   | None -> "Schema with specified name didn't found." |> AnalyzingError |> Error
--- Microsoft.OpenAPI.FunctionalExtensions/OpenApiTools.fs
+++ Microsoft.OpenAPI.FunctionalExtensions/OpenApiTools.fs
@@ -1,39 +1,12 @@
 ﻿module OpenApiTools
 
 open System
-open System.Collections
 open System.Collections.Generic
 open Microsoft.OpenApi
 open System.Text.Json.Nodes
-open Microsoft.OpenApi
 open StringExtensions
-// open Results // legacy disabled
 open SeqExtensions
-open System.IO
-open StringExtensions
-open System.Collections.Generic
 
-let rec private intersect a b =
-    match a with
-    |h::t -> match b with
-             |h2::t2 -> 
-                 if h=h2 then h::(intersect t t2)
-                 else if h>h2 then intersect t b else intersect a t2
-             |[] -> []
-    |[] -> []
-    
-let private invert x = not x
-
-//type Models.OpenApiOperation with
-//  interface System.IComparable with
-//       member x.CompareTo yobj =
-//           match yobj with
-//             | :? antiint as y -> anticompare x y
-//             | _ -> invalidArg "yobj" "cannot compare values of different types" 
-  //member s1.icompare(s2: string) = System.String.Equals(s1, s2, System.StringComparison.CurrentCultureIgnoreCase);
-//type Operation = Operation of Models.OpenApiOperation
-
-// OpenAPI v2 removed OpenApiAny model types; skip extension parsing helpers for now
 let rec tryGetValue (value: obj) : Option<string> =
   match value with
   | null -> None
@@ -53,77 +26,19 @@ let rec tryGetValue (value: obj) : Option<string> =
   | :? int as i -> Some (string i)
   | :? float as f -> Some (string f)
   | _ -> Some (value.ToString())
-  
 
-let getExtensionValue (extensions:IDictionary<string,IOpenApiExtension>) extensionName = 
+let getExtensionValue (extensions: IDictionary<string, IOpenApiExtension>) extensionName =
   match extensions, extensionName with
-  | _, extensionName when (String.IsNullOrEmpty extensionName) -> Option.None
-  | extensions, _ when extensions = null -> Option.None
+  | _, extensionName when String.IsNullOrEmpty extensionName -> None
+  | extensions, _ when extensions = null -> None
   | extensions, extensionName ->
     let isFound, foundValue = extensions.TryGetValue extensionName
     match isFound, foundValue with
-    | false, _ -> Option.None
+    | false, _ -> None
     | _, (:? JsonNodeExtension as jext) -> jext.Node :> obj |> tryGetValue
     | _, anyValue -> anyValue :> obj |> tryGetValue
-    
 
-let operationHasExtensionWithTrueValue (operation:OpenApiOperation) operationName =
-    let value = getExtensionValue operation.Extensions operationName
-    match value with
-    | Some extensionStringValue when extensionStringValue.icompare "true" -> true
-    | _ -> false
-
-(*let isInternalOperation operation =
-  operationHasExtensionWithTrueValue operation "x-vendor-product-internal"
-  
-let isNotInternalOperation: OpenApiOperation -> bool = isInternalOperation >> invert
-  
-let isImplementedOperation operation =
-  operationHasExtensionWithTrueValue operation "x-vendor-product-implemented"*)  
-  
-
-let getOperations (document:OpenApiDocument) =
-  document.Paths
-  |> Seq.map (fun pathKv -> pathKv.Value)
-  |> Seq.collect (fun item -> item.Operations)
-  |> Seq.map (fun item -> item.Value)
-  
-let getOperationsBySelector document selector =
-  document |> getOperations |> Seq.filter selector
-  
-(*let getImplementedOperations document =
-  getOperations document
-  |> Seq.where isImplementedOperation
-  
-let getInternalOperations document =
-  getOperations document
-  |> Seq.where isInternalOperation*)
-  
- 
-(*let getNotInternalImplementedOperations document =
-  getOperations document
-  |> Seq.filter isImplementedOperation
-  |> Seq.filter isNotInternalOperation*)
-
-let getSchemaByName (document:OpenApiDocument) (schemaName) =
-  document.Components.Schemas
-  |> Seq.tryFind (fun pair -> pair.Key = schemaName)
-  |> function
-    | None -> None
-    | Some pair -> (pair.Value :?> OpenApiSchema) |> Some    
-  
-let rec traverseSchema<'T> (extract: OpenApiSchema -> 'T) (schema: OpenApiSchema) (propertyName: string) : seq<'T> =
-  let currentResult = schema |> extract
-  
-  let innerPropertiesResult =
-    match schema.Properties with
-    | null -> Seq.empty
-    | properties -> properties |> Seq.collect (fun property -> traverseSchema extract (property.Value :?> OpenApiSchema) property.Key)
-    
-  let allOfResult =
-    match schema.AllOf with
-    | null -> Seq.empty
-    | allOfSchemas -> allOfSchemas |> Seq.collect (fun schema -> traverseSchema extract (schema :?> OpenApiSchema) propertyName)
-     
-  seq { yield currentResult; yield! innerPropertiesResult; yield! allOfResult }
-  
+let operationHasExtensionWithTrueValue (operation: OpenApiOperation) operationName =
+  match getExtensionValue operation.Extensions operationName with
+  | Some extensionStringValue when extensionStringValue.icompare "true" -> true
+  | _ -> false
